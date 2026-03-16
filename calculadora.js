ca7a273415537ddcd1917c6ece2bca99f8a53b78
LUIS FELIPE DREYER BENDER <06494868037@senacrs.edu.br>
2026-03-16 16:23:13 -0300

    cria funçao de divisao

--- calculadora.js
+++ calculadora.js
@@ -10,3 +10,10 @@ function subtrair(a, b) {
 function multiplicar(a, b) {
     return a * b;
 }
+
+function dividir(a, b) {
+    if (b === 0) {
+        return "Erro: Divisão por zero não é permitida.";
+    }
+    return a / b;
+} 
